f73452b5d70f594d6257a058319bbc53b8538ab5
Oliver Smith <blugammadesign@gmail.com>
2025-06-25 04:22:21 +0100

    updated token for directus

--- server/api/cms/services-dropdown.get.ts
+++ server/api/cms/services-dropdown.get.ts
@@ -3,7 +3,7 @@ import {createDirectus, rest, staticToken, readField} from '@directus/sdk';
 export default defineEventHandler( async () => {
 
     const client = createDirectus('https://cspadmin.gammaify.com')
-        .with(staticToken('58uli_X9GRbtOrXwp1PsoZ4bjREqJ-9K'))
+        .with(staticToken('xRQM1o6Wnpyfy-r7To8dR1Y5ogDLgTNp'))
         .with(rest());
     const data = await client.request(
         readField('Contacts', 'subject')
--- server/api/contact/submit.post.ts
+++ server/api/contact/submit.post.ts
@@ -2,7 +2,7 @@ import {createDirectus, rest, staticToken, createItem} from '@directus/sdk';
 export default defineEventHandler( async (event) => {
     const data = await readBody(event);
     const client = createDirectus('https://cspadmin.gammaify.com')
-        .with(staticToken('GlUkchVDK8SWu0I3AnpWHvUZvNV8CTCc'))
+        .with(staticToken('xRQM1o6Wnpyfy-r7To8dR1Y5ogDLgTNp'))
         .with(rest());
 
     const cloudflareResponse = await verifyTurnstileToken(data['cf-turnstile-response']);
