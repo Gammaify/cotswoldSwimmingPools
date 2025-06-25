9287196d34997e5fb741a2b5f49db0f3cfa03ade
Oliver Smith <blugammadesign@gmail.com>
2025-06-25 05:20:24 +0100

    updated directus token

--- server/api/cms/services-dropdown.get.ts
+++ server/api/cms/services-dropdown.get.ts
@@ -3,7 +3,7 @@ import {createDirectus, rest, staticToken, readField} from '@directus/sdk';
 export default defineEventHandler( async () => {
 
     const client = createDirectus('https://cspadmin.gammaify.com')
-        .with(staticToken('uZ9L9aZVs79uWrxajZqO9oc7WcrchW9M'))
+        .with(staticToken('3QLMY8PhhKjMPAX5z3_QRQAg7HBYtCDO'))
         .with(rest());
     const data = await client.request(
         readField('Contacts', 'subject')
