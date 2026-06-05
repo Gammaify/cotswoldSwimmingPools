b5f62c7da0c1c4da32182959d51b9d4480e90c53
Oliver Smith <blugammadesign@gmail.com>
2026-06-05 21:30:57 +0100

    testing new dokploy

--- server/api/cms/about.get.ts
+++ server/api/cms/about.get.ts
@@ -2,10 +2,10 @@ import {createDirectus, rest, staticToken, readSingleton} from '@directus/sdk';
 
 export default defineEventHandler( async () => {
 
-    const client = createDirectus('https://admin.cotswoldswimmingpools.co.uk')
-        .with(staticToken('aDC420JnsUO4uBAR9EqFgI_h71uBYWmx'))
+    const client = createDirectus('https://cspadmin.gammaify.com')
+        .with(staticToken('k203M2zvZ-88cpGGPe3YM_8LlRq1XY3H'))
         .with(rest());
-
+//aDC420JnsUO4uBAR9EqFgI_h71uBYWmx
     return await client.request(
         readSingleton("About", {
             fields: ['about_text', 'about_image_1', 'about_image_2']
